68f1290bdd9593abf6def34d66a7854f17c12788
Inspire Search Corporation <tsubasa@superai.online>
2020-12-02 17:55:00 +0900

    committed

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import Cocoa
+//let urlString: String = "https://www.tsubasakato.com"
+//let url = URL(string: urlString)
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var textURL: NSTextField!
+    @IBOutlet var HTMLView: NSTextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +26,35 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    @IBAction func button_click(_ sender: Any) {
+        let myURLString = textURL.stringValue
+        guard let myURL = URL(string: myURLString) else {
+            print("Error: \(myURLString) doesn't seem to be a valid URL")
+            return
+        }
 
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            print("HTML : \(myHTMLString)")
+            //et destinationText = myHTMLString
+            //HTMLView.documentView!.insertText(destinationText)
+            
+            //HTMLView.documentView!.insertText(myHTMLString)
+            HTMLView.string = ""
+            HTMLView.string = myHTMLString
+            //let attr = NSAttributedString(string: myURLString)
+
+            //text.appendAttributedString(attr)
+
+        } catch let error {
+            print("Error: \(error)")
+        }
+        
+        
+    }
+    
+   
+    
 
 }
 
